d229a78bcd027cad24c1e799963dfdc5893794c0
Santi Siele <santi.siele@gmail.com>
2026-08-11 14:14:28 -0300

    Add ProxyFix for HTTPS behind Railway proxy

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_session import Session
 from cachelib.file import FileSystemCache
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 import config
 from routes import register_blueprints
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 app.secret_key = config.FLASK_SECRET_KEY
 
 app.config["SESSION_TYPE"] = "cachelib"
